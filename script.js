74429aaa3b0c62c02917dcb2a7112e36a8c5071c
Kapple <ycwei98@gmail.com>
2022-10-24 00:30:21 +0800

    bg-color btn and print input btn

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function myFunction(){
 	const addH1 = document.createElement("h1")
-	addH1.innerHTML = 'Hi There!'
+	let inputValue = document.getElementById("input").value
+	addH1.innerHTML = inputValue
 	const div = document.getElementsByTagName("div")[0]
 	div.appendChild(addH1)
+}
+
+function setBGgrey(element) {
+	document.getElementsByTagName("body")[0].style.background = "grey"
+}
+
+function setBGwhite(element) {
+	document.getElementsByTagName("body")[0].style.background = ""
+}
+
+function setBGcolor(color) {
+	document.getElementsByTagName("body")[0].style.background = color
 }
