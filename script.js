ba280682a052a94c16255863032c7e02df011302
Kapple <ycwei98@gmail.com>
2022-10-24 21:28:38 +0800

    build outlines

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-function myFunction(){
-	const addH1 = document.createElement("h1")
-	let inputValue = document.getElementById("input").value
-	addH1.innerHTML = inputValue
-	const div = document.getElementsByTagName("div")[0]
-	div.appendChild(addH1)
+const changeBG = (e) => {
+	let color = window.getComputedStyle(e).backgroundColor
+	document.getElementById('pickBackground').style.background = color
 }
 
-function setBGgrey(element) {
-	document.getElementsByTagName("body")[0].style.background = "grey"
+const pickBG = (e) => {
+	let color = window.getComputedStyle(e).backgroundColor
+	document.getElementById('letter').style.background = color
 }
 
-function setBGwhite(element) {
-	document.getElementsByTagName("body")[0].style.background = ""
+const getText = () => {
+	let text =  document.getElementById('inputbox').value;
+	text = text.replaceAll('/n','<br>').replaceAll('/s', '  ');
+	document.getElementById('letterContent').innerHTML = text;
+	console.log('work!',text);
 }
 
-function setBGcolor(color) {
-	document.getElementsByTagName("body")[0].style.background = color
-}
+
+// function myFunction(){
+// 	const addH1 = document.createElement("h1")
+// 	let inputValue = document.getElementById("input").value
+// 	addH1.innerHTML = inputValue
+// 	const div = document.getElementsByTagName("div")[0]
+// 	div.appendChild(addH1)
+// }
+
+// function setBGgrey(element) {
+// 	document.getElementsByTagName("body")[0].style.background = "grey"
+// }
+
+// function setBGwhite(element) {
+// 	document.getElementsByTagName("body")[0].style.background = ""
+// }
+
+// function setBGcolor(color) {
+// 	document.getElementsByTagName("body")[0].style.background = color
+// }
