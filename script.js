2752a87c24c2714f6bc5c74614b5bf5d56432d6e
Kapple <ycwei98@gmail.com>
2022-10-30 00:18:31 +0800

    modify margin and add link to all buttons

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// When the user scrolls the page, execute navTop() and scroll()
+window.onscroll = function() {navTop(); scroll()};
+
+// stick navbar to the top
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop; // Get the offset position of the navbar
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navTop() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// back to top button
+const topBtn = document.getElementById("topBtn");
+// When the user scrolls down 400px from the top of the document, show the button
+function scroll() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+// back to top when click
+const topFunction = () => {
+ 	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 const changeBG = (e) => {
 	let color = window.getComputedStyle(e).backgroundColor
 	document.getElementById('pickBackground').style.background = color
@@ -6,6 +37,12 @@ const changeBG = (e) => {
 const pickBG = (e) => {
 	let color = window.getComputedStyle(e).backgroundColor
 	document.getElementById('letter').style.background = color
+	console.log(color)
+	if (color === 'rgb(2, 49, 94)') {
+		document.getElementById('letter').style.color = 'white'
+	} else {
+		document.getElementById('letter').style.color = 'black'
+	}
 }
 
 const getText = () => {
@@ -15,6 +52,9 @@ const getText = () => {
 	console.log('work!',text);
 }
 
+// const generateLetter = () => {
+// 	alert(document.getElementsByTagName('canvas')[0].getContext())
+// }
 
 // function myFunction(){
 // 	const addH1 = document.createElement("h1")
